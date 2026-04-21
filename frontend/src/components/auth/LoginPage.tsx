c18a6321f06ae83d39799b1042be63473698e4e9
Andy Le <Andytule321@gmail.com>
2026-04-21 02:23:24 -0400

    - Refactor code

--- frontend/src/components/auth/LoginPage.tsx
+++ frontend/src/components/auth/LoginPage.tsx
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 
-import { useAuth } from '../../context/AuthContext';
+import { useAuth } from '../../context/useAuth';
 
 const LoginPage: React.FC = () => {
   const { signInWithGoogle } = useAuth();
